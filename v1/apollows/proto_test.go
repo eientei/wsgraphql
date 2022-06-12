210deef0d64cac7ccef5a934f4298a2037cf2205
Alexander Tumin <iamtakingiteasy@eientei.org>
2022-06-12 19:19:00 +0300

    proto tests

--- v1/apollows/proto_test.go
+++ v1/apollows/proto_test.go
@@ -2,6 +2,8 @@ package apollows
 
 import (
 	"encoding/json"
+	"errors"
+	"io"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -86,4 +88,97 @@ func TestDataReadPayloadDataError(t *testing.T) {
 
 	assert.Error(t, err)
 	assert.Nil(t, pd)
+}
+
+func TestDataReadPayloadError(t *testing.T) {
+	data := Data{
+		Value: PayloadError{
+			Message: "123",
+		},
+	}
+
+	bs, err := json.Marshal(data)
+
+	assert.NoError(t, err)
+
+	var ndata Data
+
+	err = json.Unmarshal(bs, &ndata)
+
+	assert.NoError(t, err)
+
+	pd, err := ndata.ReadPayloadError()
+
+	assert.NoError(t, err)
+	assert.Equal(t, "123", pd.Message)
+}
+
+func TestDataReadPayloadErrorError(t *testing.T) {
+	var ndata *Data
+
+	pd, err := ndata.ReadPayloadError()
+
+	assert.Error(t, err)
+	assert.Nil(t, pd)
+
+	ndata = &Data{
+		Value:      nil,
+		RawMessage: json.RawMessage(`foo`),
+	}
+
+	pd, err = ndata.ReadPayloadError()
+
+	assert.Error(t, err)
+	assert.Nil(t, pd)
+}
+
+func TestDataReadPayloadErrors(t *testing.T) {
+	data := Data{
+		Value: []PayloadError{
+			{
+				Message: "123",
+			},
+		},
+	}
+
+	bs, err := json.Marshal(data)
+
+	assert.NoError(t, err)
+
+	var ndata Data
+
+	err = json.Unmarshal(bs, &ndata)
+
+	assert.NoError(t, err)
+
+	pd, err := ndata.ReadPayloadErrors()
+
+	assert.NoError(t, err)
+	assert.Len(t, pd, 1)
+	assert.Equal(t, "123", pd[0].Message)
+}
+
+func TestDataReadPayloadErrorsError(t *testing.T) {
+	var ndata *Data
+
+	pd, err := ndata.ReadPayloadErrors()
+
+	assert.Error(t, err)
+	assert.Nil(t, pd)
+
+	ndata = &Data{
+		Value:      nil,
+		RawMessage: json.RawMessage(`foo`),
+	}
+
+	pd, err = ndata.ReadPayloadErrors()
+
+	assert.Error(t, err)
+	assert.Nil(t, pd)
+}
+
+func TestWrapError(t *testing.T) {
+	err := WrapError(io.EOF, EventUnauthorized)
+
+	assert.True(t, errors.Is(err, io.EOF))
 }
